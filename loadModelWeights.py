f1f3a822837983715ff0384af20459ac1031a601
bengoodhew <bengoodhew19@gmail.com>
2026-07-25 18:28:10 +0100

    Fixed the copy in the load model weights file

--- loadModelWeights.py
+++ loadModelWeights.py
@@ -1,11 +1,10 @@
 from huggingface_hub import hf_hub_download
 from pathlib import Path
+import shutil
 
 """
     Loads model weights from the Hugging Face repo    
 """
-
-
 
 repo = "bengoodhew/transformer-nlp-models-3in1"
 
@@ -15,8 +14,7 @@ downloaded = hf_hub_download(repo_id=repo, filename="anaphora.pt")
 
 savePath = "experiments/Anaphora/models"
 Path(savePath).mkdir(parents=True, exist_ok=True)
-Path(downloaded).replace(savePath+"/modelBest.pt")
-
+shutil.copy(downloaded, savePath+"/modelBest.pt")
 
 
 
@@ -25,7 +23,7 @@ downloaded = hf_hub_download(repo_id=repo, filename="ner.pt")
 
 savePath = "experiments/Ner/models"
 Path(savePath).mkdir(parents=True, exist_ok=True)
-Path(downloaded).replace(savePath+"/modelBest.pt")
+shutil.copy(downloaded, savePath+"/modelBest.pt")
 
 
 
@@ -34,5 +32,6 @@ downloaded = hf_hub_download(repo_id=repo, filename="sentiment.pt")
 
 savePath = "experiments/Sentiment/models"
 Path(savePath).mkdir(parents=True, exist_ok=True)
-Path(downloaded).replace(savePath+"/modelBest.pt")
+shutil.copy(downloaded, savePath+"/modelBest.pt")
+
 
